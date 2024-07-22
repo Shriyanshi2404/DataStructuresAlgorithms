07df54ee8f69085540f24b0d008c460b946ad27b
Shriyanshi24 <shriyanshijindal@gmail.com>
2024-07-22 21:04:29 +0530

    LinearSearch Program

---
+++ src/com/data/structures/algorithms/Arrays/LinearSearch.java
@@ -0,0 +1,29 @@
+package com.data.structures.algorithms.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    static int findAnElement(int n, int arr[], int K) {
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] == K)
+                return 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++)
+        {
+            arr[i] = s.nextInt();
+        }
+        // element to find
+        int K = s.nextInt();
+        System.out.println(findAnElement(n, arr, K));
+    }
+}
