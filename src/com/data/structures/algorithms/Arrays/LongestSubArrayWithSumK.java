54d3c6d4fbb1f00e7100f57e319bf011702cfceb
Shriyanshi2404 <shriyanshi24jindal@gmail.com>
2024-10-06 17:40:28 +0530

    added better approach for longest sub array with sum K

--- src/com/data/structures/algorithms/Arrays/LongestSubArrayWithSumK.java
+++ src/com/data/structures/algorithms/Arrays/LongestSubArrayWithSumK.java
@@ -1,5 +1,6 @@
 package com.data.structures.algorithms.Arrays;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 // Note all array elements will be Positive
@@ -39,6 +40,50 @@ public class LongestSubArrayWithSumK {
             }
         }
         return length;
+    }
+
+    // Better approach (Hashing using prefix sum approach)
+    // T(n) = O(N)
+    // First, we will declare a map to store the prefix sums and the indices.
+    // Then we will run a loop(say i) from index 0 to n-1(n = size of the array).
+    // For each index i, we will do the following:
+    // We will add the current element i.e. a[i] to the prefix sum.
+    // If the sum is equal to k, we should consider the length of the current subarray i.e. i+1. We will compare this length with the existing length and consider the maximum one.
+    // We will calculate the prefix sum i.e. x-k, of the remaining subarray.
+    // If that sum of the remaining part i.e. x-k exists in the map, we will calculate the length i.e. i-preSumMap[x-k], and consider the maximum one comparing it with the existing length we have achieved until now.
+    // If the sum, we got after step 3.1, does not exist in the map we will add that with the current index into the map. We are checking the map before insertion because we want the index to be as minimum as possible and so we will consider the earliest index where the sum x-k has occurred. [Detailed discussion in the edge case section]
+    public static int lenOfLongSubarrUsingHashing(int A[], int N, int K) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int maxLen=0, sum=0;
+
+        for(int i=0;i<N;i++)
+        {
+            // will add current element into sum
+            sum += A[i];
+
+            // check if sum equals to K, if yes then check length and store maximum one
+            if(sum == K)
+            {
+                maxLen =  Math.max(maxLen, i+1);
+            }
+
+            // calculate prefix sum of remaining subarray
+            int prefSum = sum-K;
+
+            // if prefix sum exists in map, then check length and store maximum one
+            if(map.containsKey(prefSum))
+            {
+                int len = i - map.get(prefSum);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            // if map does not contains existing sum, then add it with current index
+            if(!map.containsKey(sum))
+            {
+                map.put(sum, i);
+            }
+        }
+        return maxLen;
     }
 
     // Optimal Approach T(N) = O(2N)
--- src/com/data/structures/algorithms/Arrays/MajorityElement.java
+++ src/com/data/structures/algorithms/Arrays/MajorityElement.java
@@ -61,7 +61,9 @@ public class MajorityElement {
     // Optimal solution
     // T(n) = O(N) + O(N)
     // Idea is Moore's voting algorithm
-    // If the array contains a majority element, its occurrence must be greater than the floor(N/2). Now, we can say that the count of minority elements and majority elements is equal up to a certain point in the array. So when we traverse through the array we try to keep track of the count of elements and the element itself for which we are tracking the count.
+    // If the array contains a majority element, its occurrence must be greater than the floor(N/2).
+    // Now, we can say that the count of minority elements and majority elements is equal up to a certain point in the array.
+    // So when we traverse through the array we try to keep track of the count of elements and the element itself for which we are tracking the count.
     // After traversing the whole array, we will check the element stored in the variable.
     // If the question states that the array must contain a majority element, the stored element will be that one
     // but if the question does not state so, then we need to check if the stored element is the majority element or not. If not, then the array does not contain any majority element.
