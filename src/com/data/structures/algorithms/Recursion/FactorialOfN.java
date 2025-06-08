6e7621de54bf79a28f86eefe81740a5a3bf55321
Shriyanshi2404 <shriyanshi.jindal@walmart.com>
2025-06-08 16:13:41 +0530

    added factorial program

---
+++ src/com/data/structures/algorithms/Recursion/FactorialOfN.java
@@ -0,0 +1,35 @@
+package com.data.structures.algorithms.Recursion;
+
+import java.util.Scanner;
+
+public class FactorialOfN {
+    public static int factorial(int n)
+    {
+        if(n == 1)
+            return 1;
+
+        return n * factorial(n-1);
+    }
+
+    public static void findFactorial(int i, int ans)
+    {
+        if(i == 1)
+        {
+            System.out.print("Factorial of using parameterised way is : " + ans);
+            return;
+        }
+        findFactorial(i-1, ans * i);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // using fucntional way
+        System.out.println("Factorial of "+ n + " is : " + factorial(n));
+
+        // using parameterised way
+        findFactorial(n, 1);
+    }
+}
